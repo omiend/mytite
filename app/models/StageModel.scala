960aaf07b7740c7e756b4d55f8d739b6f15c8451
omiend <omiend@gmail.com>
2014-04-07 23:42:13 +0900

    Models修正

--- app/models/StageModel.scala
+++ app/models/StageModel.scala
@@ -1,6 +1,14 @@
+package models
+
+import java.util.Date
+import play.api.db._
+import play.api.Play.current
+import anorm._
+import anorm.SqlParser._
+
 /** Stage Master */
 case class Stage (
-	id: Pk[Long]
+	 id: Pk[Long]
 	,stageName:String
 	,createDate: Option[Date]
 	,updateDate: Option[Date]
