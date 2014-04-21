33060a8e7a9209278c1dbf0470cc0868cecc42ab
omiend <omiend@gmail.com>
2014-04-21 21:42:21 +0900

    バグ修正

--- app/models/StageModel.scala
+++ app/models/StageModel.scala
@@ -10,10 +10,53 @@ import anorm.SqlParser._
 case class Stage (
 	 id: Pk[Long]
 	,stageName:String
+	,sort:String
+	,color:String
 	,createDate: Option[Date]
 	,updateDate: Option[Date]
 ) {
 }
 
-object Stage {	
+object Stage {
+
+	/**
+   * Stage Simple
+   */
+  val simple = {
+    get[Pk[Long]]("id") ~
+    get[String]("stage_name") ~
+    get[String]("sort") ~
+    get[String]("color") ~
+    get[Date]("create_date") ~
+    get[Date]("update_date") map {
+      case id~stageName~sort~color~createDate~updateDate => 
+      Stage(
+         id
+        ,stageName
+        ,sort
+        ,color
+        ,Option(createDate)
+        ,Option(updateDate)
+      )
+    }
+  }
+
+  /**
+   * Stage from-toで件数を指定して取得
+   */
+  def findAll: Seq[Stage] = {
+    DB.withConnection { implicit connection =>
+      // 親テーブル取得
+      val resultList: Seq[Stage] = SQL(
+        """
+        select *
+          from stage
+         order by sort
+        """
+      ).as(
+        Stage.simple *
+      )
+      resultList
+    }
+  }
 }
