ac2deca8380d051585f64433fe83eadbca522fa4
omiend <omiend@gmail.com>
2014-04-29 19:57:47 +0900

    FestivalとStageを一緒に登録する処理を追加

--- app/models/StageModel.scala
+++ app/models/StageModel.scala
@@ -9,6 +9,7 @@ import anorm.SqlParser._
 /** Stage Master */
 case class Stage (
 	 id: Pk[Long]
+  ,festivalId: Long
 	,stageName:String
 	,sort:String
 	,color:String
@@ -24,14 +25,16 @@ object Stage {
    */
   val simple = {
     get[Pk[Long]]("id") ~
+    get[Long]("festival_id") ~
     get[String]("stage_name") ~
     get[String]("sort") ~
     get[String]("color") ~
     get[Date]("create_date") ~
     get[Date]("update_date") map {
-      case id~stageName~sort~color~createDate~updateDate => 
+      case id~festivalId~stageName~sort~color~createDate~updateDate => 
       Stage(
          id
+        ,festivalId
         ,stageName
         ,sort
         ,color
@@ -57,6 +60,41 @@ object Stage {
         Stage.simple *
       )
       resultList
+    }
+  }
+
+  /**
+   * Stage Insert処理
+   */
+  def insart(stage: Stage) {
+    DB.withConnection { implicit connection =>
+      SQL(
+        """
+          insert into stage(
+             festival_id
+            ,stage_name
+            ,sort
+            ,color
+            ,create_date
+            ,update_date
+          ) values (
+             {festival_id}
+            ,{stage_name}
+            ,{sort}
+            ,{color}
+            ,{create_date}
+            ,{update_date}
+          )
+        """
+      ).on(
+         'festival_id -> stage.festivalId
+        ,'stage_name  -> stage.stageName
+        ,'sort        -> stage.sort
+        ,'color       -> stage.color
+        ,'create_date -> stage.createDate
+        ,'update_date -> stage.updateDate
+      ).executeInsert()
+
     }
   }
 }
--- app/models/TimeTable.scala
+++ app/models/TimeTable.scala
@@ -4,28 +4,26 @@ import java.util.Date
 import play.api.Play.current
 
 /** TimeTable Structure */
-case class TimeTable  (
-   /** 出演時間 */
-   var timeLabel: String
+case class TimeTable (
+   /** フェスの名前(Festival.name) */
+   var festivalName: String
    /** ステージ */
   ,var stageList: Seq[String] = Seq.empty
-   /** PerformanceMap（キーはステージ名） */
-  ,var timeTableStageMap: Map[String, Performance] = Map.empty
 ) {
 
-  /**
-   * [画面用] ステージ順にPerformanceをリストで返却する
-   */
-  def getPerformanceList: Seq[Performance] = {
-    timeTableStageMap.values.toSeq
-  }
+  // /**
+  //  * [画面用] ステージ順にPerformanceをリストで返却する
+  //  */
+  // def getPerformanceList: Seq[Performance] = {
+  //   timeTableStageMap.values.toSeq
+  // }
 
-  /**
-   * 指定したステージ名を削除する
-   */
-  def deleteStageName(deleteStageName: String) {
-    stageList.filter { _ != deleteStageName }
-  }
+  // /**
+  //  * 指定したステージ名を削除する
+  //  */
+  // def deleteStageName(deleteStageName: String) {
+  //   stageList.filter { _ != deleteStageName }
+  // }
 }
 
 object TimeTable {
